33274f7aa033cd8e44e84da9bc42152d91122f32
Mutuku Mutua <mutua.mutuku@student.moringaschool.com>
2022-11-24 02:06:24 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Mutuku Mutua';
+const welcomeMessage = 'Welcome to Flatbook, ' + 'Mutuku Mutua!';
+const  excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + "MUTUKU MUTUA!";
+const  shortGreeting = "Welcome, " + "Mutuku!"
